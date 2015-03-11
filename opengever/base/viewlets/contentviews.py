58f98ac4be8d1e647a995d9fe992b2685042d261
David Erni <david.erni@4teamwork.ch>
2015-03-11 15:15:06 +0100

    WIP: investigate, move checkin-checkout menu to content-views-viewlet.

--- opengever/base/viewlets/contentviews.py
+++ opengever/base/viewlets/contentviews.py
@@ -1,4 +1,13 @@
+from opengever.base.interfaces import ICheckinCheckoutMenuItem
 from plone.app.layout.viewlets.common import ContentViewsViewlet
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.browsermenu.interfaces import IBrowserSubMenuItem
+from zope.browsermenu.menu import getMenu
+from zope.component import getAdapters
+from zope.interface import Interface
+from zope.interface import providedBy
+from zope.interface.interfaces import IInterface
+from zope.security.proxy import removeSecurityProxy
 
 
 class ModelContentViewsViewlet(ContentViewsViewlet):
@@ -6,6 +15,7 @@ class ModelContentViewsViewlet(ContentViewsViewlet):
 
     For plone content fall-back to default content views.
     """
+    index = ViewPageTemplateFile('contentviews.pt')
 
     def prepareObjectTabs(self, default_tab='view', sort_first=('folderContents',)):
         """Prepare the object tabs by determining their order and working
@@ -36,3 +46,44 @@ class ModelContentViewsViewlet(ContentViewsViewlet):
             'link_target': None,
             'id': 'edit'
         },)
+
+    def menu(self):
+        result = []
+        for name, item in getAdapters((self.context, self.request), ICheckinCheckoutMenuItem):
+            if item.available():
+                result.append(item)
+
+        # Now order the result. This is not as easy as it seems.
+        #
+        # (1) Look at the interfaces and put the more specific menu entries
+        #     to the front.
+        # (2) Sort unambigious entries by order and then by title.
+        ifaces = list(providedBy(removeSecurityProxy(self.context)).__iro__)
+        max_key = len(ifaces)
+        def iface_index(item):
+            iface = item._for
+            if not iface:
+                iface = Interface
+            if IInterface.providedBy(iface):
+                return ifaces.index(iface)
+            if isinstance(removeSecurityProxy(self.context), item._for):
+                # directly specified for class, this goes first.
+                return -1
+            # no idea. This goes last.
+            return max_key
+        result = [(iface_index(item), item.order, item.title, item)
+                  for item in result]
+        result.sort()
+
+        result = [
+            {'title': title,
+             'description': item.description,
+             'action': item.action,
+             'selected': (item.selected() and u'selected') or u'',
+             'icon': item.icon,
+             'extra': item.extra,
+             'submenu': (IBrowserSubMenuItem.providedBy(item) and
+                         getMenu(item.submenuId, self.context, self.request)) or None}
+            for index, order, title, item in result]
+
+        return result
